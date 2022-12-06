45350f48231315cf6d150d5ebdaab0015aaab506
Inctinct <113181582+Inctinct@users.noreply.github.com>
2022-12-06 14:51:10 +0300

    Update settings.py

--- library/library/settings.py
+++ library/library/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'library_db',
         'USER': 'postgres',
-        'PASSWORD': '252515Mdd',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
